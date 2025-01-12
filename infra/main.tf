9b774b5cddb1cac564a32fc95d955e21b78e88d8
Emanuel Rocha <emanuel.rochax@gmail.com>
2025-01-12 11:24:40 -0300

    ECS Deploymento adjustments

--- infra/main.tf
+++ infra/main.tf
@@ -179,6 +179,8 @@ resource "aws_ecs_service" "service" {
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.task.arn
   desired_count   = 1
+  deployment_minimum_healthy_percent = 50 
+  deployment_maximum_percent         = 200
   launch_type     = "EC2"
 }
 
